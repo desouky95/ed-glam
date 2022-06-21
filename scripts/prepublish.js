f9b8f127314e0f39af9162efbee1489ecc948a30
Ismail Moustafa <tubesniper@gmail.com>
2022-06-21 15:20:03 +0200

    Update prepublish.js

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,8 +1,16 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 
 const { program } = require("commander");
 const { getPkgPath } = require("../utils/paths");
-const { rmdir, rm, readdirSync, rmdirSync, rmSync, existsSync } = require("fs");
+const {
+  rmdir,
+  rm,
+  readdirSync,
+  rmdirSync,
+  rmSync,
+  existsSync,
+  copyFileSync,
+} = require("fs");
 const { glob } = require("glob");
 const path = require("path");
 let __pkgPath = "";
@@ -10,12 +18,16 @@ program.option("-p, --path", "Package Path");
 const options = program.opts();
 
 program.action(async () => {
-  const files = glob.sync("packages/**".replace(/\\/g, "/"), {});
-  files.forEach((_) => {
-    if (existsSync(path.resolve(_) && existsSync(path.resolve(`${_}/build`)))) {
-      copyFileSync(`${_}/package.json`, `${_}/build/package.json`);
-
-      //   rmSync(path.resolve(_), { force: true, recursive: true, maxRetries: 1 });
+  const dirs = readdirSync("./packages", { withFileTypes: true })
+    .filter((_) => _.isDirectory())
+    .map((_) => path.resolve(`./packages/${_.name}`).replace(/\\/g, "/"));
+  dirs.forEach((pathToResolve) => {
+   
+    if (existsSync(pathToResolve) && existsSync(`${pathToResolve}/build`)) {
+      copyFileSync(
+        `${pathToResolve}/package.json`,
+        `${pathToResolve}/build/package.json`
+      );
     }
   });
 });
