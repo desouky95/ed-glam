6f9ca90e6ee50dfcaf234ecfd9430e363cb4913c
desouky <tubesniper@gmail.com>
2022-08-01 18:07:20 +0200

    fix modal

---
+++ packages/ed-components/src/Modal/Modal.stories.tsx
@@ -0,0 +1,30 @@
+import { FlexLayout } from '@eduact/ed-system';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useState } from 'react';
+import { RaisedButton } from '..';
+import Modal from './Modal';
+
+export default { title: 'Modal', component: Modal } as ComponentMeta<
+	typeof Modal
+>;
+
+export const ModalStory: ComponentStory<typeof Modal> = (args) => {
+	const [open, setOpen] = useState(false);
+	return (
+		<div>
+			<RaisedButton onClick={() => setOpen(true)}>Open</RaisedButton>
+			<Modal
+				open={open}
+				onClose={() => setOpen(false)}
+				withBackdrop
+				withStyling
+			>
+				<FlexLayout
+					style={{ background: 'yellowgreen' }}
+					height="20vh"
+					width="50vh"
+				></FlexLayout>
+			</Modal>
+		</div>
+	);
+};
--- packages/ed-components/src/Modal/Modal.tsx
+++ packages/ed-components/src/Modal/Modal.tsx
@@ -8,6 +8,7 @@ export type ModalProps = {
 	withBackdrop?: boolean;
 	onClose?: () => void;
 	withStyling?: boolean;
+	withDelay?: boolean;
 } & PortalProps;
 
 const Modal: React.FC<ModalProps> = ({
@@ -16,7 +17,7 @@ const Modal: React.FC<ModalProps> = ({
 	onClose,
 	withBackdrop = true,
 	withStyling = false,
-
+	withDelay = true,
 	...props
 }) => {
 	const modalRef = useRef(null);
@@ -26,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({
 	});
 	return (
 		<Portal {...props}>
-			{(open || delayed) && (
+			{(open || (delayed && withDelay)) && (
 				<>
 					{withStyling && (
 						<Backdrop
