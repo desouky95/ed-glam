8cbcd114a89ab2fbbdd7d2b09ede836c40664667
Ismail Moustafa <tubesniper@gmail.com>
2022-11-19 16:55:18 +0200

    add analytics more icons

--- packages/ed-components/src/Icons/Icons.stories.tsx
+++ packages/ed-components/src/Icons/Icons.stories.tsx
@@ -15,31 +15,89 @@ export const IconsDefault: ComponentStory<typeof Icon> = (args) => {
 	return (
 		<FlexLayout flexWrap={'wrap'} gridGap="1rem">
 			<Icon {...args}>
-				<Icons.ChevronLeft />
-			</Icon>
-			<Icon color="orange" {...args}>
-				<Icons.ChevronRight />
-			</Icon>
-			<Icon {...args}>
-				<Icons.Warning />
-			</Icon>
-			<Icon {...args}>
-				<Icons.DoubleArrowUp />
-			</Icon>
-			<Icon {...args}>
-				<Icons.RightArrow />
-			</Icon>
-			<Icon {...args}>
 				<Icons.Activity />
 			</Icon>
 			<Icon {...args}>
-				<Icons.User />
+				<Icons.AppRegistration />
+			</Icon>
+
+			<Icon {...args}>
+				<Icons.ArrowForward />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Bolt />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Bookmark />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Check />
+			</Icon>
+			<Icon {...args}>
+				<Icons.ChevronLeft />
+			</Icon>
+			<Icon {...args}>
+				<Icons.ChevronLess />
+			</Icon>
+			<Icon {...args}>
+				<Icons.ChevronMore />
+			</Icon>
+			<Icon {...args}>
+				<Icons.ChevronRight />
+			</Icon>
+			<Icon {...args}>
+				<Icons.CircleCheck />
+			</Icon>
+			<Icon {...args}>
+				<Icons.ClassBookmark />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Close />
 			</Icon>
 			<Icon {...args}>
 				<Icons.Dashboard />
 			</Icon>
 			<Icon {...args}>
+				<Icons.DoubleArrowUp />
+			</Icon>
+			<Icon {...args}>
+				<Icons.EyeIcon />
+			</Icon>
+			<Icon {...args}>
+				<Icons.EyeoffIcon />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Filter />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Magnifier />
+			</Icon>
+			<Icon {...args}>
+				<Icons.NotStarted />
+			</Icon>
+			<Icon {...args}>
+				<Icons.RightArrow />
+			</Icon>
+			<Icon {...args}>
 				<Icons.Settings />
+			</Icon>
+			<Icon {...args}>
+				<Icons.SwapVertically />
+			</Icon>
+			<Icon {...args}>
+				<Icons.User />
+			</Icon>
+			<Icon {...args}>
+				<Icons.UserRejected />
+			</Icon>
+			<Icon {...args}>
+				<Icons.UserVerified />
+			</Icon>
+			<Icon {...args}>
+				<Icons.Warning />
+			</Icon>
+			<Icon {...args}>
+				<Icons.WideClose />
 			</Icon>
 		</FlexLayout>
 	);
