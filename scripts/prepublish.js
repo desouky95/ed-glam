720e856aab9d1cca5fcc3ed10910d23d461d4c43
Ismail Moustafa <tubesniper@gmail.com>
2023-03-08 20:01:08 +0200

    fix prepublish script.

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -3,24 +3,56 @@ import { program } from 'commander';
 import { createRequire } from 'module';
 import { readdirSync, existsSync, copyFileSync } from 'fs';
 import path from 'path';
+import fse from 'fs-extra';
 const require = createRequire(import.meta.url);
 const { glob } = require('glob');
 let __pkgPath = '';
 program.option('-p, --path', 'Package Path');
 const options = program.opts();
 
+async function createPackageFile(packagePath, buildFolder) {
+	const packageData = await fse.readFile(
+		path.resolve(packagePath, './package.json'),
+		'utf8'
+	);
+	const { nyc, scripts, devDependencies, workspaces, ...packageDataOther } =
+		JSON.parse(packageData);
+
+	const buildPath = path.join(packagePath, `./${buildFolder}`);
+
+	const newPackageData = {
+		...packageDataOther,
+		private: false,
+		...(packageDataOther.main
+			? {
+					main: './index.js',
+					module: './index.js',
+			  }
+			: {}),
+		types: './index.d.ts',
+	};
+
+	const targetPath = path.resolve(buildPath, './package.json');
+
+	await fse.writeFile(
+		targetPath,
+		JSON.stringify(newPackageData, null, 2),
+		'utf8'
+	);
+	console.log(`Created package.json in ${targetPath}`);
+
+	return newPackageData;
+}
 program.action(async () => {
 	const dirs = readdirSync('./packages', { withFileTypes: true })
 		.filter((_) => _.isDirectory())
 		.map((_) => path.resolve(`./packages/${_.name}`).replace(/\\/g, '/'));
-	dirs.forEach((pathToResolve) => {
+	for (let index = 0; index < dirs.length; index++) {
+		const element = array[index];
 		if (existsSync(pathToResolve) && existsSync(`${pathToResolve}/build`)) {
-			copyFileSync(
-				`${pathToResolve}/package.json`,
-				`${pathToResolve}/build/package.json`
-			);
+			await createPackageFile(pathToResolve, 'build');
 		}
-	});
+	}
 });
 
 program.parse(process.argv);
